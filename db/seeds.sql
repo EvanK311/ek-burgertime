e96c5047d860def567826c1c780c9619c923a858
Evan K <EvanK311@gmail.com>
2020-10-15 20:56:34 -0700

    skeleton done with displaying functional site

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Chee');
+INSERT INTO burgers (burger_name) VALUES ('White Cheddar Onion');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Garlic Cheddar', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Jackson', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Bitin', true);
+INSERT INTO burgers (burger_name) VALUES ('Cat-su Burger');
